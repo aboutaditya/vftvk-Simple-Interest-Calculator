03bd4715de9dc35344ef278c648d78fe231a086b
Aditya Thakur <100947528+aboutaditya@users.noreply.github.com>
2022-05-01 00:11:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ function compute()
     var par = document.getElementById("principal").value;
     if(par<=0){
         alert("Enter a Positive number")
-        document.getElementById("principal").focus();
-
     }
 
     else{
@@ -14,7 +12,7 @@ function compute()
         var i= p*y*r/100;
         var year=new Date().getFullYear()+parseInt(y);
         var a=i;
-        document.getElementById("result").innerHTML="If you deposit "+p+",\<br\>at an interest rate of "+r+"%\<br\>You will receive an amount of "+a+",\<br\>in the year "+year+"\<br\>"
+        document.getElementById("result").innerHTML="If you deposit <mark>"+p+"</mark>,\<br\>at an interest rate of <mark>"+r+"</mark>%\<br\>You will receive an amount of <mark>"+a+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
     }
 }
 function ratec(){
